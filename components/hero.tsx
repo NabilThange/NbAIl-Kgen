6769432633592923ed08418d8972d08ceb45ffc5
NabilThange <thangenabil@gmail.com>
2025-04-15 23:43:26 +0530

    feat: Replace 'Learn More' button with arrow style

--- components/hero.tsx
+++ components/hero.tsx
@@ -147,19 +147,14 @@ export default function Hero() {
               </button>
             </Link>
             <Link href="#features">
-              <button className="animated-button">
-                <svg xmlns="http://www.w3.org/2000/svg" className="arr-2" viewBox="0 0 24 24">
-                  <path
-                    d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-                  ></path>
+              {/* From Uiverse.io by reshades */}
+              <button className="arrow-button">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"></path>
                 </svg>
-                <span className="text">Learn More</span>
-                <span className="circle"></span>
-                <svg xmlns="http://www.w3.org/2000/svg" className="arr-1" viewBox="0 0 24 24">
-                  <path
-                    d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-                  ></path>
-                </svg>
+                <div className="text">
+                  Learn More
+                </div>
               </button>
             </Link>
           </motion.div>
